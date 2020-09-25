3235f9c339a6aceed9879d0e16a1d7e79031f1a4
Mikle <michaels17525@mail.ru>
2020-09-25 16:43:45 +0300

    Second try

--- proj/settings.py
+++ proj/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
